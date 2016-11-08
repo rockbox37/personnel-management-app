6d0aa0a2908d36279e09dda7a4a1011e8a8e504f
Stephen Go <stephengo@outlook.com>
2016-11-08 17:23:01 -0500

    table populating from pre-generated data

--- app/routes/personnel.js
+++ app/routes/personnel.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
-
+let personnel = [{
+  id: '134',
+  firstName: 'Mike',
+  lastName: 'X',
+  yrs: '5'
+}, {
+  id: '345',
+  firstName: 'Test',
+  lastName: 'New',
+  yrs: '7'
+}]
 export default Ember.Route.extend({
+  model() {
+    return personnel;
+  }
 });
